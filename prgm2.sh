36f802a3c1517b866f81e25c70331ddb7cc01460
anithalaya <anithakoyikkal2@gmail.com>
2023-01-18 15:42:32 +0530

    basic math

---
+++ prgm2.sh
@@ -0,0 +1,14 @@
+echo "Enter two numbers"
+read a
+read b
+s=` expr $a + $b`
+echo "sum is $s"
+m=` expr $a - $b`
+echo "difference is $m"
+p=` expr $a \* $b`
+echo "product is $s"
+d=` expr $a / $b`
+echo "division is $s"
+
+
+
